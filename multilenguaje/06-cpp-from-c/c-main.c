8b210608ab6341aca9228ce41e7e3f86f70fe493
ale <ale@wtpc>
2017-03-03 20:06:53 -0300

    listo el problema de llamar a c++ desde c

--- multilenguaje/06-cpp-from-c/c-main.c
+++ multilenguaje/06-cpp-from-c/c-main.c
@@ -1,6 +1,5 @@
-
 #include <stdio.h>
-#include "c-sum.h"
+#include "cpp-sum.h"
 
 #define NUM 200
 
--- multilenguaje/06-cpp-from-c/cpp-sum.h
+++ multilenguaje/06-cpp-from-c/cpp-sum.h
@@ -1,7 +1,6 @@
-
 #ifndef C_SUM_H
 #define C_SUM_H
 
-int sum_abs(const int * const in, const int num);
+extern "C" {int sum_abs(const int * const in, const int num);}
 
 #endif
