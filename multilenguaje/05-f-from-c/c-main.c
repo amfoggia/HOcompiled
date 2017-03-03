2e995747e178c3310205274d191188c8dd5d32dd
ale <ale@wtpc>
2017-03-03 14:46:42 -0300

    otra cosa

--- multilenguaje/05-f-from-c/c-main.c
+++ multilenguaje/05-f-from-c/c-main.c
@@ -6,13 +6,13 @@
 
 int main(int argc, char **argv)
 {
-    int data[NUM], num, i;
+    int data[NUM], num, i,z;
 
     num = NUM;
     for (i=0; i < num; i++) {
         data[i] = i - 100 + 1;  /* NOTE: difference in array numbering */  
     }
-
-    printf("sum=%d\n", sum_abs(data, num));
+    sum_abs_(data, &num, &z);
+    printf("sum=%d\n", z);
     return 0;
 }
