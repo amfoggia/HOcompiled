6ae7c15a1839ea0be61524a608cc1ed07c038413
ale <ale@wtpc>
2017-03-03 15:34:47 -0300

    lista la parte de llamar a c desde fortran

--- multilenguaje/04-c-from-f/c-sum.c
+++ multilenguaje/04-c-from-f/c-sum.c
@@ -1,12 +1,11 @@
 #include <stdlib.h>
 #include "c-sum.h"
 
-int sum_abs(int *in, int num) {
-   int i,sum;
-
-   for (i=0,sum=0; i < num; ++i) {
-       sum += abs(in[i]);
+void sum_abs_(int *in, int *num,int *sum) {
+   int i;
+   *sum=0;
+   for (i=0; i < *num; ++i) {
+       *sum += abs(in[i]);
    }
-   return sum;
 }
  
